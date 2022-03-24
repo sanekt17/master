1bf69b674276ac58e8d1c92f54a34aa2c5bef12d
sanekt17 <92449936+sanekt17@users.noreply.github.com>
2022-03-24 22:12:53 +0200

    Add files via upload

---
+++ h.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# download azure-cli and azcopy
+sudo rpm --import <https://packages.microsoft.com/keys/microsoft.asc>
+echo -e "[azure-cli]
+name=Azure CLI
+baseurl=https://packages.microsoft.com/yumrepos/azure-cli
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/azure-cli.repo
+sudo yum install azure-cli
+wget https://aka.ms/downloadazcopy-v10-linux
+tar -xvf downloadazcopy-v10-linux
+sudo cp ./azcopy_linux_amd64_*/azcopy /usr/bin/
